cbd5d6db4a3f5d5bd127951f30a0d059e5bf5e5b
Eric Douglas <eric.douglas.mail@gmail.com>
2015-08-09 20:28:01 -0300

    Add fixture data

--- app/models/quote.js
+++ app/models/quote.js
@@ -5,4 +5,22 @@ var attr = DS.attr;
 export default DS.Model.extend({
   message: attr('string'),
   author: attr('string')
+}).reopenClass({
+  FIXTURES: [
+    {
+      message: 'The mind is everything. What you think you become.',
+      author: 'Buddha',
+      id: 1
+    },
+    {
+      message: "If you can't explain it simply, you don't understand it well enough.",
+      author: 'Albert Einstein',
+      id: 2
+    },
+    {
+      message: "Our prime purpose in this life is to help others. And if you can't help them, at least don't hurt them.",
+      author: 'Dalai Lama',
+      id: 3
+    },
+  ]
 });
