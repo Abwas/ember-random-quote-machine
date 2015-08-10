96c422436f4573fc369d7d1fc7a6f9e093435ede
Eric Douglas <eric.douglas.mail@gmail.com>
2015-08-10 14:00:24 -0300

    Create a quote and redirect to /all

--- app/routes/new.js
+++ app/routes/new.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    createQuote() {
+      let quote = {
+        author: this.controllerFor('new').get('author'),
+        message: this.controllerFor('new').get('message')
+      };
+      let newQuote = this.store.createRecord('quote', quote);
+
+      this.controllerFor('new').set('author', '');
+      this.controllerFor('new').set('message', '');
+      
+      newQuote.save();
+      this.transitionTo('all');
+    }
+  }
 });
